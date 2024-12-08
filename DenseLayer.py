dc32282d1b098c5ba58bc5fed279d042d777e722
Iizalaarab Elhaimeur <ielha003@odu.edu>
2024-12-08 14:52:32 -0500

    Exploring Randomly Optimizing Weights and Biases

--- DenseLayer.py
+++ DenseLayer.py
@@ -12,6 +12,7 @@ class Layer_Dense:
 
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
+        self.inputs = inputs
 
 class Activation_ReLU:
     def forward(self, inputs):
@@ -62,11 +63,40 @@ activation2.forward(dense2.output)
 
 
 loss_function = Loss_CategoricalCrossEntropy()
-loss = loss_function.calculate(activation2.output, y_train)
 
+lowest_loss = 9999999
+best_dense1_weights = dense1.weights.copy()
+best_dense1_biases = dense1.biases.copy()
+best_dense2_weights = dense2.weights.copy()
+best_dense2_biases = dense2.biases.copy()
 
-print(loss)
+for iteration in range(10000):
+    dense1.weights += 0.05 * np.random.randn(1024,32)
+    dense1.biases += 0.05 * np.random.randn(1,32)
+    dense2.weights += 0.05 * np.random.randn(32,10)
+    dense2.biases += 0.05 * np.random.randn(1,10)
 
+    dense1.forward(X_train)
+    activation1.forward(dense1.output)
+    dense2.forward(activation1.output)
+    activation2.forward(dense2.output)
+
+    loss = loss_function.calculate(activation2.output, y_train)
+
+    if loss < lowest_loss:
+        print('New Set of Weights and Biases Found, Iteration: ', iteration,
+              'loss: ', loss)    
+        best_dense1_weights = dense1.weights.copy()
+        best_dense1_biases = dense1.biases.copy()
+        best_dense2_weights = dense2.weights.copy()
+        best_dense2_biases = dense2.biases.copy()
+        lowest_loss = loss
+    
+    else:
+        dense1.weights = best_dense1_weights.copy()
+        dense1.biases = best_dense1_biases.copy()
+        dense2.weights = best_dense2_weights.copy()
+        dense2.biases = best_dense2_biases.copy()
 
 # weights1 = [[0.2, 0.8, -0.5, 1.0],
 #             [0.5, -0.91, 0.26, -0.5],
